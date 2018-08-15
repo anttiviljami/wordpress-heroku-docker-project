246c40d9fbdcf314ba500b157a835df89e5bb511
Viljami Kuosmanen <viljami.kuosmanen@futurice.com>
2018-08-15 13:05:02 +0300

    Script improvements

--- tools/migrate-db.sh
+++ tools/migrate-db.sh
@@ -1,8 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 USAGE="migrate-db.sh <source env> <target env>"
+
+DUMP_HEADER="
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+"
+
+DUMP_FOOTER="
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+"
 
 # Check args
 if [ -z "$1" ] || [ -z "$2" ]; then
@@ -33,9 +52,9 @@ fi
 
 # Pipe search & replaced mysqldump from source to target
 if [[ $1 == 'local' ]]; then
-  EXPORT_CMD="docker-compose run -T --rm shell bash -c '. .env && wp search-replace $SOURCE_URL $TARGET_URL --export'"
+  EXPORT_CMD="docker-compose run -T --rm shell bash -c '. .env && wp search-replace $SOURCE_URL $TARGET_URL --export --all-tables'"
 else
-  EXPORT_CMD="heroku run --no-tty 'wp search-replace $SOURCE_URL $TARGET_URL --export' -r $1"
+  EXPORT_CMD="heroku run --no-tty 'wp search-replace $SOURCE_URL $TARGET_URL --export --all-tables' -r $1"
 fi
 
 if [[ $2 == 'local' ]]; then
@@ -44,7 +63,7 @@ else
   IMPORT_CMD="heroku run --no-tty 'wp db import -' -r $2"
 fi
 
-eval "$EXPORT_CMD | $IMPORT_CMD"
+eval "(echo -e '$DUMP_HEADER' && $EXPORT_CMD && echo -e '$DUMP_FOOTER') | $IMPORT_CMD"
 
 # Flush cache in target
 if [[ $2 == 'local' ]]; then
@@ -52,7 +71,6 @@ if [[ $2 == 'local' ]]; then
 else
   heroku run wp cache flush -r $2
 fi
-
 
 echo "Success!"
 
