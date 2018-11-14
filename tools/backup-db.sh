242ce99115790189bd95d60ce89ebab973482736
Viljami Kuosmanen <viljami@avoinsorsa.fi>
2018-11-14 12:31:20 +0200

    Use local mysqldump instead of heroku one
    Piping connections through heroku dynos isn't reliable for backups

--- tools/backup-db.sh
+++ tools/backup-db.sh
@@ -3,6 +3,8 @@
 set -e
 
 USAGE="backup-db.sh <source env>"
+DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+BACKUP_TARGET=backup-$1-$(date +"%Y-%m-%d").sql
 
 
 # Check args
@@ -12,13 +14,23 @@ if [ -z "$1" ]; then
 fi
 
 if [[ $1 == 'local' ]]; then
-  EXPORT_CMD="docker-compose run -T --rm shell bash -c '. .env && mysqldump \$(php tools/database-url-mysqlopts.php \$DATABASE_URL) --quick --single-transaction --extended-insert'"
+  DB_URL=$DATABASE_URL
 else
-  EXPORT_CMD="heroku run --no-tty 'mysqldump \$(php tools/database-url-mysqlopts.php \$DATABASE_URL) --quick --single-transaction --extended-insert' -r $1"
+  echo "Getting DATABASE_URL from heroku..."
+  DB_URL=$(heroku config:get DATABASE_URL -r $1)
 fi
 
-BACKUP_TARGET=backup-$1-$(date +"%Y-%m-%d").sql
+CONNECTION_OPTS=$(php $DIRNAME/database-url-mysqlopts.php $DB_URL)
+EXPORT_CMD="(
+mysqldump $CONNECTION_OPTS \
+  --quick \
+  --single-transaction \
+  --extended-insert \
+  --ignore_table=wordpress.cache_products 2> /dev/null && \
+mysqldump --no-data $CONNECTION_OPTS cache_products 2> /dev/null
+)"
 
+echo "Backing up to database to file $BACKUP_TARGET with mysqldump..."
 eval "$EXPORT_CMD > $BACKUP_TARGET"
 echo "Success! Backed up to file $BACKUP_TARGET"
 
